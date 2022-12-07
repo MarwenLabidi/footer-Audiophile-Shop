c2bad0815eab661e678ff854dc3902d7cc906476
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-07 14:18:47 +0100

    💄 style app mobile

--- src/setup/styled-components/styled_component.js
+++ src/setup/styled-components/styled_component.js
@@ -1,38 +1,72 @@
 import styled, { css } from "styled-components";
 
 export const StyledFooter = styled.footer`
-        height: 45vh;
+        height: 85vh;
         background-color: var(--color-primary);
         color: var(--color-white);
+        text-align: center;
         /* if is it mobile change the height */
-        ${({ isMobile }) =>
-                isMobile &&
+        ${({ isBrowser }) =>
+                isBrowser &&
                 css`
-                        height: 80vh!important;
+                        height: 45vh;
                 `}
-
-        &.firstSection {
-                &.orangeBarLogoSection {
-                        &.orangeBar {
+        & .firstSection {
+                & .orangeBarLogoSection {
+                        & .orangeBar {
+                                height: 5px;
+                                width: 150px;
+                                background-color: var(--color-secondary);
+                                margin: auto;
+                                margin-bottom: 20px;
                         }
                         & .logo {
+                                margin: auto;
+                                width: 180px;
                         }
                 }
                 & nav {
+                        /* background-color:red; */
                         & ul {
+                                margin: 0;
+                                padding: 30px 0;
                                 & li {
+                                        padding: 5px;
+                                }
+                                letter-spacing: 0.125em;
+                                & li:hover,
+                                & li:focus {
+                                        cursor: pointer;
+                                        color: var(--color-secondary);
+                                        transition: all 0.3s ease-in-out;
                                 }
                         }
                 }
         }
-        &.secondeSection {
+        & .secondeSection {
+                /* background-color:red; */
+                /* height: 100%; */
                 & article {
+                        color: var(--color-gray-400);
                         & p {
+                                padding: 0 30px;
                         }
                         & .copyRight {
+                                margin: 30px 0;
+                                font-size: 1.2rem;
                         }
                 }
                 & .logoSection {
+                        & svg {
+                                margin: 10px;
+                                fill: var(--color-white);
+                        }
+                        & svg:hover,
+                        & svg:focus {
+                                cursor: pointer;
+                                fill: var(--color-secondary);
+                                transition: all 0.3s ease-in-out;
+                        }
                 }
         }
 `;
