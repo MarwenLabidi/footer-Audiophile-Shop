e654d1078f514af9bf231066502530a531c9ceca
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-07 14:55:33 +0100

    💄 style app

--- src/setup/styled-components/styled_component.js
+++ src/setup/styled-components/styled_component.js
@@ -1,15 +1,19 @@
 import styled, { css } from "styled-components";
 
 export const StyledFooter = styled.footer`
-        height: 85vh;
+        height: 88vh;
         background-color: var(--color-primary);
         color: var(--color-white);
         text-align: center;
+	display: grid;
+	align-content: center;
+	/* align-items: center; */
         /* if is it mobile change the height */
         ${({ isBrowser }) =>
                 isBrowser &&
                 css`
-                        height: 45vh;
+                        height: 50vh;
+			align-content: space-between;
                 `}
         & .firstSection {
                 & .orangeBarLogoSection {
@@ -29,7 +33,7 @@ export const StyledFooter = styled.footer`
                         /* background-color:red; */
                         & ul {
                                 margin: 0;
-                                padding: 30px 0;
+                                padding: 20px 0;
                                 & li {
                                         padding: 5px;
                                 }
@@ -40,21 +44,37 @@ export const StyledFooter = styled.footer`
                                         color: var(--color-secondary);
                                         transition: all 0.3s ease-in-out;
                                 }
+				${({ isBrowser }) =>
+                isBrowser &&
+                css`
+			display: flex;
+			gap: 10px;
+                `}
                         }
                 }
+		${({ isBrowser }) =>
+                isBrowser &&
+                css`
+			display: flex;
+			justify-content: space-around;
+                `}
         }
         & .secondeSection {
-                /* background-color:red; */
-                /* height: 100%; */
                 & article {
                         color: var(--color-gray-400);
                         & p {
                                 padding: 0 30px;
                         }
                         & .copyRight {
-                                margin: 30px 0;
                                 font-size: 1.2rem;
+				margin: 20px 0;
                         }
+			${({ isBrowser }) =>
+                isBrowser &&
+                css`
+			/* background-color:red; */
+			width:40%;
+                `}
                 }
                 & .logoSection {
                         & svg {
@@ -68,5 +88,12 @@ export const StyledFooter = styled.footer`
                                 transition: all 0.3s ease-in-out;
                         }
                 }
+		${({ isBrowser }) =>
+                isBrowser &&
+                css`
+			display: flex;
+			justify-content: space-around;
+			text-align: left;
+                `}
         }
 `;
